4a3e0b53cd0c3b25f431b951777065e3a4c0b946
sameerthor <sameer@thor.solutions>
2025-05-12 15:48:39 +0530

    session key found

--- main.js
+++ main.js
@@ -223,6 +223,8 @@ async function automateMCAProcess(win, companyID) {
     return await win.webContents.executeJavaScript(`
       (function() {
         try {
+        console.log('SessionStorage Keys:', Object.keys(sessionStorage));
+
           const details = sessionStorage.getItem("companyDetails");
           return details ? JSON.parse(details) : null;
         } catch (err) {
